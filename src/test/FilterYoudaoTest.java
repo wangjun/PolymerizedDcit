a6795f9e7b563a67663ee5a9a5c15fa51571fa62
byd <byandong@gmail.com>
2014-03-10 21:47:10 +0800

    add youdao.com openapi support

---
+++ src/test/FilterYoudaoTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.polymerized.bean.MetaMeaning;
+import com.polymerized.pagefilter.FilterYoudao;
+
+public class FilterYoudaoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetMeansBykewword() {
+		List<MetaMeaning> list = new FilterYoudao().getMeansBykewword("shi");
+		Iterator<MetaMeaning> ite = list.iterator();
+		while (ite.hasNext())
+			System.out.println(ite.next());
+	}
+
+}
